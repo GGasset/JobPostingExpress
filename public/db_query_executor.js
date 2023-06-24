61e7f3c254ff1b5696d184c4a1c9a9e8ab310864
Germán <GassetGerman@gmail.com>
2023-06-24 21:12:19 +0200

    finished creating database structure

--- public/db_query_executor.js
+++ public/db_query_executor.js
@@ -1,12 +1,34 @@
+// query queue item example
+/*
+    {
+        "query": "SELECT * FROM users",
+        "req": req,
+        "output": Promise
+        
+        // Promise output
+        [
+            {
+                "id": 0,
+                "username": "...",
+                "password_hash": "...",
+                ...
+            },
+            {
+                ...
+            },
+            ...
+        ]
+    }
+*/
 let query_queue = [];
 
 module.exports = query_queue;
 
 new Promise((resolve, reject) => {
     while (true) {
-        if (query_queue.length > 0)
-        {
-            
-        }
+        if (query_queue.length === 0)
+            continue;
+
+        
     }
 });
