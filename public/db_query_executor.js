49a8df5307e18fced20b82d779d3b20b561c38fa
Germán <GassetGerman@gmail.com>
2023-06-25 21:00:28 +0200

    finished db_query_executor

--- public/db_query_executor.js
+++ public/db_query_executor.js
@@ -1,9 +1,12 @@
+const sqlite = require('sqlite3').verbose();
+
 // query queue item example
 /*
     {
         "query": "SELECT * FROM users",
         "req": req,
-        "output": Promise
+        "res": res,
+        "output": Promise // No need to provide it.
         
         // Promise output
         [
@@ -24,11 +27,33 @@ let query_queue = [];
 
 module.exports = query_queue;
 
-new Promise((resolve, reject) => {
+new  Promise(async (resolve, reject) => {
+    const db = new sqlite.Database('../models/db.db');
     while (true) {
         if (query_queue.length === 0)
             continue;
 
-        
+        let copied = false;
+        query_queue[0]['output'] = new Promise((resolve, reject) => {
+            let query_object = query_queue[0];
+            copied = true;
+            console.log(`${query_object.res.statusCode} ${query_object.req.path} ${query_object.query}`);
+            db.all(query_object['query'], (err, rows) => {
+                if (err)
+                    reject(err);
+
+                resolve(rows);
+            });
+        })
+
+        while (!copied)
+        {
+            await sleep(10);
+        }
+        query_queue.shift();
     }
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
