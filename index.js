c9c6167a01d472caa15aa8034e7a05e88d14be3b
Germán <GassetGerman@gmail.com>
2023-06-25 22:22:06 +0200

    Started index.html and its server side counterpart

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const html_sanitizer = require('sanitize-html');
 
 const app = express();
 
@@ -10,4 +11,10 @@ nunjucks.configure('views', {
 });
 
 // Session configuration (Credentials
-app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}));
+app.use(session({secret:"fingerpint", resave: true, saveUninitialized: true}));
+
+app.get('/', (req, res) => {
+	res.render('index.html', {
+		"req": req,
+	});
+})
