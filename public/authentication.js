4bc929e2bbae106146df0f011058700452c2abe6
Germán <GassetGerman@gmail.com>
2023-06-28 16:18:27 +0200

    finished functions

--- public/authentication.js
+++ public/authentication.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 
 const is_authenticated = (req) => {
-    
+    let result = false;
+    if (!req.session.authorization)
+        return false
+
+    jwt.verify(req.session.authorization.accessToken, process.env.JWTSecret, (err, user) => {
+        if (err)
+            result = false;
+        else
+            result = true;
+    })
+    return result;
+}
+
+const require_authentication = (req, res) => {
+    if (!is_authenticated(req))
+    {
+        res.status(403).send();
+        return false;
+    }
+    return true
+}
+
+const get_user_email = (req) => {
+    return req.session.authorization.email;
 }
 
 module.exports.is_authenticated = is_authenticated;
+module.exports.require_authentication = require_authentication;
+module.exports.get_user_email = get_user_email;
