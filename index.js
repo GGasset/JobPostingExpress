b96586bfda1663aacd183237a0092176022bd9b1
Germán <GassetGerman@gmail.com>
2023-07-17 17:01:47 +0200

    added comment

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.use(function(req, res, next) {
+	// If user.is_company get user data
 	new Promise(async function(resolve, reject) {
 		if (!authentication_functions.is_authenticated(req))
 		{
