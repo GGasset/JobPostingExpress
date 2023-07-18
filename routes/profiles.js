e5e9f9f350c95b8e269fbff4dcc7a523a8e2bc29
Germán <GassetGerman@gmail.com>
2023-07-18 21:52:33 +0200

    added basics for profiles router

---
+++ routes/profiles.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const authentication = require('../public/server_side/authentication');
+
+const profile_router = express.Router();
+
+profile_router.use('/me', function(req, res, next) {
+    if (!authentication.is_authenticated(req)) {
+        res.redirect('/session/login');
+    }
+})
+
+profile_router.get('/me', function(req, res) {
+    res.render('user_settings.html');
+})
+
+profile_router.use('/me/company', function(req, res, next) {
+    if (!authentication.require_company_authentication(req, res))
+    {
+        return false;
+    }
+    else if (!req.session.user.is_company_admin)
+    {
+        res.status(403).send();
+        return;
+    }
+    next();
+})
+
+profile_router.get('/me/company', function(req, res) {
+    res.render('company_settings.html');
+})
