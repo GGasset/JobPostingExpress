96be02faa0d8e8cb5c0bac10cb9149233b845c0e
Germán <GassetGerman@gmail.com>
2023-09-26 08:16:39 +0200

    updated package.json started with url structure at jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const db = require('../models/db');
 
 const job_router = express.Router();
 
-
+job_router.get('jobs/:category', (req, res) => {
+    new Promise((resolve, reject) => {
+        
+    })
+});
 
 module.exports = job_router;
