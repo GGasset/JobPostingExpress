829def3f60b8a8d86b37c4436a36ad730253855c
Germán <GassetGerman@gmail.com>
2023-10-13 22:10:42 +0200

    added API route to get user basic data

--- routes/API.js
+++ routes/API.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-
+const db = require("../models/db");
 
 const API_router = express.Router();
 
@@ -12,6 +11,20 @@ const job_posting_router = require("./API/job_posting");
 API_router.use(likes_router);
 API_router.use(messaging_router);
 API_router.use(job_posting_router);
+
+
+API_router.get("user_info/:is_company/:id", (req, res) => {
+    new Promise(async (resolve, reject) => {
+        const is_company = req.params.is_company;
+        const user_id = req.params.id;
+
+        let user_info = is_company ? await db.get_company_info(user_id) : await db.get_user_info_by_id(user_id);
+        user_info.email = undefined;
+        return user_info;
+    }).then(user_info => {
+        res.status(200).send(JSON.stringify(user_info));
+    });
+});
 
 
 module.exports = API_router;
